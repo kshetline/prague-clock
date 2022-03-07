03abce7838e60148a903e9e54f81cd739f5155c3
Kerry Shetline <kerry@shetline.com>
2022-03-07 05:29:08 -0500

    Adjust base SVG to 50N latitude. Handle outer ring and Bohemian hours.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { abs, cos_deg, floor, mod } from '@tubular/math';
 import { DateTimeStyle, TimeEditorOptions } from '@tubular/ng-widgets';
-import { MOON, SkyObserver, SolarSystem, SUN } from '@tubular/astronomy';
+import { EventFinder, MOON, SET_EVENT, SkyObserver, SolarSystem, SUN } from '@tubular/astronomy';
 import ttime, { DateTime, utToTdt } from '@tubular/time';
 import julianDay = ttime.julianDay;
 
@@ -13,14 +13,15 @@ import julianDay = ttime.julianDay;
 export class AppComponent implements OnInit {
   private baseMoonAngle: number;
   private baseSunAngle: number;
-  private observer = new SkyObserver(14.4208, 50.088);
+  private eventFinder = new EventFinder();
+  private observer = new SkyObserver(14.4185, 50.0870);
+  private solarSystem = new SolarSystem();
   private _time = Date.now();
 
   handAngle = 0;
   moonAngle = 0;
   outerRingAngle = 0;
   siderealAngle = 0;
-  solarSystem = new SolarSystem();
   sunAngle = 0;
   zone = 'Europe/Prague';
 
@@ -49,16 +50,21 @@ export class AppComponent implements OnInit {
   updateTime(): void {
     const jdu = julianDay(this.time);
     const jde = utToTdt(jdu);
+    const sunsetA = this.eventFinder.findEvent(SUN, SET_EVENT, jdu, this.observer, undefined, undefined, true);
+    const sunsetB = this.eventFinder.findEvent(SUN, SET_EVENT, sunsetA.ut, this.observer, undefined, undefined, false);
+    const dayLength = sunsetB.ut - sunsetA.ut;
+    const bohemianHour = (jdu - sunsetA.ut) / dayLength * 24;
     const date = new DateTime(this.time, this.zone);
     const wt = date.wallTime;
+    const hourOfDay = wt.hour + wt.minute / 60 - wt.dstOffset / 3600;
 
     this.baseSunAngle = this.solarSystem.getEclipticPosition(SUN, jde).longitude.degrees;
     this.baseMoonAngle = this.solarSystem.getEclipticPosition(MOON, jde).longitude.degrees;
-    // @ts-ignore
-    this.handAngle = (wt.hour * 60 + wt.minute - wt.dstOffset / 60) / 4 - 180;
+    this.handAngle = hourOfDay * 15 - 180;
     this.sunAngle = 90 - this.baseSunAngle + cos_deg(this.baseSunAngle) * 26.6;
     this.moonAngle = 90 - this.baseMoonAngle + cos_deg(this.baseMoonAngle) * 26.6;
-    this.siderealAngle = this.observer.getLocalHourAngle(jdu, false).degrees - 90;
+    this.siderealAngle = this.observer.getLocalHourAngle(jdu, true).degrees - 90;
+    this.outerRingAngle = 180 - (bohemianHour - hourOfDay) * 15;
   }
 
   rotate(angle: number): string {
