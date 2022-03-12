e3b11e03b131a4b93a9b71dedfeeb8a323db3b18
Kerry Shetline <kerry@shetline.com>
2022-03-12 17:08:27 -0500

    Generate variable day-area mask for latitude.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { abs, cos_deg, floor, mod, sin_deg, tan_deg } from '@tubular/math';
-import { DateTimeStyle, TimeEditorOptions } from '@tubular/ng-widgets';
+import { abs, acos_deg, cos_deg, floor, max, min, mod, Point, sign, sin_deg, sqrt, tan_deg } from '@tubular/math';
+import { AngleStyle, DateTimeStyle, TimeEditorOptions } from '@tubular/ng-widgets';
 import { EventFinder, MOON, SET_EVENT, SkyObserver, SolarSystem, SUN } from '@tubular/astronomy';
 import ttime, { DateTime, utToTdt } from '@tubular/time';
 import julianDay = ttime.julianDay;
 
 const CLOCK_RADIUS = 250;
 const INCLINATION = 23.5;
+const HORIZON_RADIUS = CLOCK_RADIUS * tan_deg((90 - INCLINATION) / 2);
+const TROPIC_RADIUS = HORIZON_RADIUS * tan_deg((90 - INCLINATION) / 2);
 
 interface CircleAttributes {
   cy: number;
   d?: string;
   r: number;
+}
+
+function circleIntersections(x1: number, y1: number, r1: number, x2: number, y2: number, r2: number): Point[] {
+  if (x1 === x2 && y1 === y2 && r1 === r2)
+    return null;
+  else if (r1 === 0 && r2 === 0)
+    return [];
+
+  const d = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+
+  if (d === 0 || d > r1 + r2 || d < abs(r1 - r2))
+    return [];
+
+  const a = (r1 ** 2 - r2 ** 2 + d ** 2) / 2 / d;
+  const h = sqrt(r1 ** 2 - a ** 2);
+  const x3 = x1 + a * (x2 - x1) / d;
+  const y3 = y1 + a * (y2 - y1) / d;
+
+  if (h === 0)
+    return [{ x: x3, y: y3 }];
+
+  return [
+    { x: x3 + h * (y2 - y1) / d, y: y3 - h * (x2 - x1) / d },
+    { x: x3 - h * (y2 - y1) / d, y: y3 + h * (x2 - x1) / d }
+  ];
+}
+
+function RO(n: number): string {
+  return n.toFixed(1).replace(/\.0$/, '');
 }
 
 @Component({
@@ -20,6 +51,8 @@ interface CircleAttributes {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  DD = AngleStyle.DD;
+
   LOCAL_OPTS: TimeEditorOptions = {
     dateTimeStyle: DateTimeStyle.DATE_AND_TIME,
     twoDigitYear: false,
@@ -31,12 +64,20 @@ export class AppComponent implements OnInit {
   private baseMoonAngle: number;
   private baseSunAngle: number;
   private eventFinder = new EventFinder();
-  private observer = new SkyObserver(14.4185, 50.0870);
+  private _latitude = 50.0870;
+  private _longitude = 14.4185;
+  private observer: SkyObserver;
   private solarSystem = new SolarSystem();
   private _time = Date.now();
 
+  darkCy: number;
+  darkR: number;
+  dayAreaMask: string;
   disableDst = true;
   handAngle = 0;
+  horizonCy: number;
+  horizonPath: string;
+  horizonR: number;
   isoFormat = false;
   moonAngle = 0;
   outerRingAngle = 0;
@@ -45,7 +86,16 @@ export class AppComponent implements OnInit {
   zone = 'Europe/Prague';
 
   ngOnInit(): void {
+    this.adjustLatitude();
     this.setNow();
+  }
+
+  get latitude(): number { return this._latitude; }
+  set latitude(newValue: number) {
+    if (this._latitude !== newValue) {
+      this._latitude = newValue;
+      this.adjustLatitude();
+    }
   }
 
   get time(): number { return this._time; }
@@ -59,6 +109,39 @@ export class AppComponent implements OnInit {
   setNow(): void {
     this.time = floor(Date.now() / 60000) * 60000;
     this.updateTime();
+  }
+
+  private adjustLatitude(): void {
+    this.observer = new SkyObserver(this._longitude, this._latitude);
+    ({ cy: this.horizonCy, d: this.horizonPath, r: this.horizonR } = this.getAltitudeCircle(0, true));
+    ({ cy: this.darkCy, r: this.darkR } = this.getAltitudeCircle(-18));
+    this.createDayAreaMask();
+  }
+
+  private createDayAreaMask(): void {
+    const outerPoints = circleIntersections(0, 0, CLOCK_RADIUS, 0, this.horizonCy, this.horizonR);
+    const innerPoints = circleIntersections(0, 0, TROPIC_RADIUS, 0, this.horizonCy, this.horizonR);
+
+    if (!outerPoints || outerPoints.length < 2 || !innerPoints || innerPoints.length < 2) {
+      console.log(outerPoints, innerPoints);
+      this.dayAreaMask = '';
+      return;
+    }
+
+    const x1 = RO(outerPoints[0].x);
+    const y1 = RO(outerPoints[0].y);
+    const r2 = RO(this.horizonR);
+    const x2 = RO(innerPoints[0].x);
+    const y2 = RO(innerPoints[0].y);
+    const r3 = RO(TROPIC_RADIUS);
+    const x3 = RO(innerPoints[1].x);
+    const y3 = RO(innerPoints[1].y);
+    const r4 = RO(this.horizonR);
+    const x4 = RO(outerPoints[1].x);
+    const y4 = RO(outerPoints[1].y);
+    const r5 = RO(CLOCK_RADIUS);
+
+    this.dayAreaMask = `M${x1} ${y1}A${r2} ${r2} 0 0 0 ${x2} ${y2}A${r3} ${r3} 0 0 0 ${x3} ${y3}A${r4} ${r4} 0 0 0 ${x4} ${y4}A${r5} ${r5} 0 1 1 ${x1} ${y1}`;
   }
 
   updateTime(): void {
@@ -94,17 +177,17 @@ export class AppComponent implements OnInit {
     return `M0 -12.0A12.0 12.0 0 0 ${largeArcFlag} 0 12.0A${x} 12.0 0 0 ${sweepFlag} 0 -12.0`;
   }
 
-  getAltitudeCircle(alt: number, doPath = false): CircleAttributes {
-    const lat = this.observer.latitude.degrees;
-    const r0 = CLOCK_RADIUS * tan_deg((90 - INCLINATION) / 2);
+  private getAltitudeCircle(alt: number, doPath = false): CircleAttributes {
+    const l = this.observer.latitude.degrees;
+    const lat = abs(l) > 0.01 ? l : (sign(l) || 1) * 0.01;
     const theta1 = -lat - (90 + alt);
     const theta2 = -lat + (90 + alt);
-    const x1 = r0 * sin_deg(theta1);
-    const y1 = r0 * cos_deg(theta1);
-    const x2 = r0 * sin_deg(theta2);
-    const y2 = r0 * cos_deg(theta2);
-    const ya = y1 * (r0 / (r0 - x1));
-    const yb = y2 * (r0 / (r0 - x2));
+    const x1 = HORIZON_RADIUS * sin_deg(theta1);
+    const y1 = HORIZON_RADIUS * cos_deg(theta1);
+    const x2 = HORIZON_RADIUS * sin_deg(theta2);
+    const y2 = HORIZON_RADIUS * cos_deg(theta2);
+    const ya = y1 * (HORIZON_RADIUS / (HORIZON_RADIUS - x1));
+    const yb = y2 * (HORIZON_RADIUS / (HORIZON_RADIUS - x2));
     const cy = (ya + yb) / 2;
     const r = (yb - ya) / 2;
 
@@ -113,5 +196,30 @@ export class AppComponent implements OnInit {
       d: doPath && `M 0 ${cy} m ${-r} 0 a ${r},${r} 0 1,1 ${r * 2},0 a ${r},${r} 0 1,1 ${-r * 2},0`,
       r
     };
+  }
+
+  /* private */ getHourCircle(hour: number): string {
+    const theta_unequal_hours = (r: number): number => {
+      const arg = (r ** 2 + this.horizonCy ** 2 - this.horizonR ** 2) / (2 * r * this.horizonCy);
+      if (arg <= -1)
+        return 180;
+      else if (arg >= 1)
+        return 0;
+
+      return acos_deg(arg);
+    };
+
+    let d = '';
+
+    for (let r = max(TROPIC_RADIUS, this.horizonR - this.horizonCy); r <= CLOCK_RADIUS; r += 1) {
+      const r_1 = min(r + 1, CLOCK_RADIUS);
+      const theta0 = theta_unequal_hours(r);
+      const theta1 = theta_unequal_hours(r_1);
+      const psi0 = theta0 + (360 - 2 * theta0) / 12 * hour;
+      const psi1 = theta1 + (360 - 2 * theta1) / 12 * hour;
+      d += `M ${r * sin_deg(psi0)} ${r * cos_deg(psi0)} L ${r * sin_deg(psi1)} ${r * cos_deg(psi1)}`;
+    }
+
+    return d;
   }
 }
