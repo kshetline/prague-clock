6107d4ea67d0f2cd1d427ada7f1ac220a32758d2
Kerry Shetline <kerry@shetline.com>
2022-04-09 13:54:28 -0400

    Move app.module.ts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,8 +15,8 @@ import { ToastModule } from 'primeng/toast';
 import { TooltipModule } from 'primeng/tooltip';
 import { TubularNgWidgetsModule } from '@tubular/ng-widgets';
 
-import { AppComponent } from './app.component';
-import { TimezoneSelectorComponent } from '../timezone-selector/timezone-selector.component';
+import { AppComponent } from './app/app.component';
+import { TimezoneSelectorComponent } from './timezone-selector/timezone-selector.component';
 
 @NgModule({
   declarations: [
