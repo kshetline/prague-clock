743767268112ab2e6611750ce970766eefe46de2
Kerry Shetline <kshetline@campsystems.com>
2022-04-19 13:36:19 -0400

    Refactor SVG graphics generation to separate file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ConfirmationService, MenuItem, MessageService, PrimeNGConfig } from 'primeng/api';
-import {
-  abs, atan2_deg, atan_deg, cos_deg, floor, max, min, mod, mod2, PI, sign, sin_deg, sqrt, tan_deg
-} from '@tubular/math';
+import { abs, cos_deg, floor, max, min, mod, mod2 } from '@tubular/math';
 import {
   clone, extendDelimited, forEach, getCssValue, isEqual, isLikelyMobile, isObject, isSafari, processMillis
 } from '@tubular/util';
@@ -17,32 +15,20 @@ import { TzsLocation } from '../timezone-selector/timezone-selector.component';
 import { Globe } from '../globe/globe';
 import { basePath, languageList, localeSuffix, SOUTH_NORTH, specificLocale, WEST_EAST } from '../locales/locale-info';
 import { faForward, faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
-import { AdvancedOptionsComponent, Appearance, SettingsHolder, Timing } from '../advanced-options/advanced-options.component';
+import { AdvancedOptionsComponent, Appearance, SettingsHolder, Timing }
+  from '../advanced-options/advanced-options.component';
 import {
-  adjustForEclipticWheel, BasicPositions, calculateBasicPositions, calculateMechanicalPositions, circleIntersections,
-  findCircleRadius, MILLIS_PER_DAY, solarSystem, ZeroAngles
+  adjustForEclipticWheel, BasicPositions, calculateBasicPositions, calculateMechanicalPositions, MILLIS_PER_DAY,
+  solarSystem, ZeroAngles
 } from 'src/math/math';
+import { adjustGraphicsForLatitude, SvgHost } from 'src/svg/svg';
 
 const { DATE, DATETIME_LOCAL, julianDay, TIME } = ttime;
 
-const CLOCK_RADIUS = 250;
-const INCLINATION = 23.5;
-const ARCTIC = 90 - INCLINATION;
-const LABEL_RADIUS = 212;
-const EQUATOR_RADIUS = 164.1;
-const HORIZON_RADIUS = CLOCK_RADIUS * tan_deg((90 - INCLINATION) / 2);
-const TROPIC_RADIUS = HORIZON_RADIUS * tan_deg((90 - INCLINATION) / 2);
-const MAX_UNEVEN_HOUR_LATITUDE = 86;
 const RESUME_FILTERING_DELAY = 1000;
 const START_FILTERING_DELAY = 500;
 const STOP_FILTERING_DELAY = isSafari() ? 1000 : 3000;
 const RECOMPUTED_WHEN_NEEDED: null = null;
-
-interface CircleAttributes {
-  cy: number;
-  d?: string;
-  r: number;
-}
 
 enum EventType { EQUISOLSTICE, MOON_PHASE, RISE_SET }
 enum PlaySpeed { NORMAL, FAST }
@@ -100,7 +86,7 @@ function removeOldestLocation(locations: TzsLocation[]): TzsLocation[] {
   return locations;
 }
 
-function bpKey(key: string): boolean { return !key.startsWith('_'); }
+function basicPosKey(key: string): boolean { return !key.startsWith('_'); }
 
 function formatTimeOfDay(hours: number | DateTime | DateAndTime, force24 = false, zeroIs24 = false): string {
   if (hours instanceof DateTime)
@@ -132,7 +118,7 @@ languageList.forEach(language => menuLanguageList.push({ label: language.name, u
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, SettingsHolder {
+export class AppComponent implements OnInit, SettingsHolder, SvgHost {
   faForward = faForward;
   faPlay = faPlay;
   faStop = faStop;
@@ -624,7 +610,7 @@ export class AppComponent implements OnInit, SettingsHolder {
       requestAnimationFrame(this.playStep);
   }
 
-  clearItem(index: number): void {
+  clearLocationItem(index: number): void {
     if (this.placeName === this.recentLocations[index].name)
       this.placeName = '';
 
@@ -799,144 +785,12 @@ export class AppComponent implements OnInit, SettingsHolder {
   private adjustLatitude(): void {
     this.graphicsRateChangeCheck();
     this.clearZoneFixTimeout();
-
-    this.southern = (this._latitude < 0);
-    this.rotateSign = (this.southern ? -1 : 1);
-    this.clearTimingReferenceIfNeeded();
     this.updateObserver();
+    this.clearTimingReferenceIfNeeded();
+    adjustGraphicsForLatitude(this);
     this.placeName = '';
-    ({ cy: this.horizonCy, d: this.horizonPath, r: this.horizonR } = this.getAltitudeCircle(0, true));
-    ({ cy: this.darkCy, r: this.darkR } =
-      this.getAltitudeCircle(this.appearance === Appearance.ORIGINAL_1410 ? -10 : -18));
-
-    const absLat = abs(this._latitude);
-    const excessLatitude = absLat - ARCTIC;
-
-    if (excessLatitude < 0) {
-      this.midnightSunR = 0;
-      this.solNoctisPath = '';
-      this.createDayAreaMask(CLOCK_RADIUS);
-    }
-    else {
-      this.midnightSunR = this.horizonR + this.horizonCy - 1E-4;
-
-      const r = (this.midnightSunR + CLOCK_RADIUS) / 2;
-      const x1 = cos_deg(105) * r;
-      const y1 = sin_deg(105) * r;
-      const x2 = cos_deg(75) * r;
-      const y2 = sin_deg(75) * r;
-
-      this.solNoctisPath = `M ${x1} ${y1} A ${r} ${r} 0 0 0 ${x2} ${y2}`;
-      this.createDayAreaMask(this.midnightSunR);
-    }
-
-    if (this.outerSunriseAngle != null && absLat <= MAX_UNEVEN_HOUR_LATITUDE) {
-      for (let h = 1; h <= 11; ++h) {
-        this.hourArcs[h] = this.getHourArc(h);
-        this.hourWedges[h] = this.getHourArc(h, true);
-      }
-
-      this.sunriseLabelPath = this.getHourArc(0.5);
-      this.sunsetLabelPath = this.getHourArc(11.5, false, true);
-
-      const top = (this.horizonCy - this.horizonR + this.darkCy - this.darkR) / 2;
-      const bottom = (this.horizonCy + this.horizonR + this.darkCy + this.darkR) / 2;
-      const r = (this.horizonR + this.darkR) / 2;
-      const leftArc = `M 0 ${bottom} A ${r} ${r} 0 0 1 0 ${top}`;
-      const rightArc = `M 0 ${top} A ${r} ${r} 0 0 1 0 ${bottom}`;
-      const pathLength = r * PI;
-      const labelShift = 250 - cos_deg(this._latitude) * 70;
-
-      this.dawnLabelPath = this.southern ? rightArc : leftArc;
-      this.dawnTextOffset = this.southern ? labelShift : pathLength - labelShift;
-      this.duskLabelPath = this.southern ? leftArc : rightArc;
-      this.duskTextOffset = this.southern ? pathLength - labelShift : labelShift;
-
-      if (excessLatitude <= 0) {
-        this.hourStroke = 2;
-        this.riseSetFontSize = '15px';
-      }
-      else {
-        this.hourStroke = 1;
-        this.riseSetFontSize = (cos_deg(absLat) * 37.6).toFixed(1) + 'px';
-      }
-    }
-    else {
-      this.hourArcs = [];
-      this.hourStroke = 2;
-      this.hourWedges = [];
-      this.dawnLabelPath = this.duskLabelPath = this.sunriseLabelPath = this.sunsetLabelPath = '';
-    }
-
-    const hourLabels = document.getElementById('unequalHourLabels') as unknown as SVGGElement;
-    const pts = circleIntersections(0, 0, LABEL_RADIUS, 0, this.horizonCy, this.horizonR);
-    const hAdj1 = [0, 3, -3, -7, -9, -9, -9, -12, -14, -13, -9, -3, 5];
-    const vAdj1 = [0, 30, 27, 23, 19, 16, 12, 9, 3, -4, -9, -14, -17];
-    const hAdj2 = [0, 15, 12, 0, -12, -20, -9, -5, -5, 0, 0, 8, 20];
-    const vAdj2 = [0, 30, 27, 42, 38, 25, 12, 9, 6, 5, -5, -14, -24];
-
-    if (this.outerSunriseAngle == null || !pts || pts.length < 2 || absLat > 74)
-      hourLabels.innerHTML = '';
-    else {
-      const sunrise = atan2_deg(pts[0].y, pts[0].x);
-      const step = (180 + sunrise * 2) / 12;
-      let angle = -180 - sunrise + step;
-      let html = '';
-
-      for (let h = 1; h <= 12; ++h, angle += step) {
-        const x = cos_deg(angle) * LABEL_RADIUS;
-        const y = sin_deg(angle) * LABEL_RADIUS;
-        let hAdj = hAdj1[h];
-        let vAdj = vAdj1[h];
-        let fontSize = 30;
-
-        if (absLat > ARCTIC) {
-          if (h === 1)
-            hAdj = 0.0555555556 * absLat ** 3 - 11.55555557 * absLat ** 2 + 801.5416677 * absLat - 18521.50002;
-          else if (h === 12)
-            hAdj = 0.1666666669 * absLat ** 3 - 34.50000004 * absLat ** 2 + 2379.958336 * absLat - 54688.87507;
-          else
-            hAdj = hAdj2[h];
-
-          vAdj = vAdj2[h];
-          fontSize = 20;
-        }
-        else if (absLat > 50) {
-          const wgt = (ARCTIC - absLat) / (ARCTIC - 50);
-
-          hAdj = hAdj * wgt + hAdj2[h] * (1 - wgt);
-          vAdj = vAdj * wgt + vAdj2[h] * (1 - wgt);
-          fontSize = 30 * wgt + 20 * (1 - wgt);
-        }
-
-        html += `<text x="${x}" y="${y}" dx="${hAdj}" dy="${vAdj}" class="unequalHourText"`;
-
-        if (fontSize !== 30 && (h < 4 || h > 9))
-          html += ` style="font-size: ${fontSize}px"`;
-
-        html += `>${this.southern ? 13 - h : h}</text>`;
-      }
-
-      hourLabels.innerHTML = html;
-    }
-
-    this.adjustDawnDuskGradient();
     this.updateTime(true);
     this.updateGlobe();
-  }
-
-  private adjustDawnDuskGradient(): void {
-    // Adjust radial gradient based on the rough distance between the horizon circle and then
-    // absolute night circle, in comparison to the horizon circle radius.
-    const gp1 = (circleIntersections(0, 0, EQUATOR_RADIUS, 0, this.horizonCy, this.horizonR) ?? [])[0];
-    const gp2 = (circleIntersections(0, 0, EQUATOR_RADIUS, 0, this.darkCy, this.darkR) ?? [])[0];
-    let span = this.horizonR / 3;
-
-    if (gp1 && gp2)
-      span = sqrt((gp2.x - gp1.x) ** 2 + (gp2.y - gp1.y) ** 2);
-
-    span = min(span, this.horizonR - this.darkR);
-    this.duskGradientAdjustment = max(min((1 - span / this.horizonR) * 100, 99.6), 80);
   }
 
   private graphicsRateChangeCheck(suppressFilteringImmediately = false): void {
@@ -976,56 +830,6 @@ export class AppComponent implements OnInit, SettingsHolder {
     this.globe.orient(this._longitude, this.latitude).finally();
   }
 
-  private createDayAreaMask(outerR: number): void {
-    let inner = TROPIC_RADIUS;
-
-    if (outerR !== CLOCK_RADIUS) {
-      const deltaLat = 90 - 2 * atan_deg(outerR / CLOCK_RADIUS);
-      inner = TROPIC_RADIUS * tan_deg((90 + deltaLat) / 2);
-    }
-
-    let outerPoints = circleIntersections(0, 0, outerR, 0, this.horizonCy, this.horizonR);
-    const equatorPoints = circleIntersections(0, 0, EQUATOR_RADIUS, 0, this.horizonCy, this.horizonR);
-    let innerPoints = circleIntersections(0, 0, inner, 0, this.horizonCy, this.horizonR);
-
-    if (!outerPoints || outerPoints.length < 2)
-      outerPoints = circleIntersections(0, 0, outerR - 1E-6, 0, this.horizonCy, this.horizonR);
-
-    if (!innerPoints || innerPoints.length < 2)
-      innerPoints = circleIntersections(0, 0, inner + 1E-6, 0, this.horizonCy, this.horizonR);
-
-    if (!outerPoints || outerPoints.length < 2 || !innerPoints || innerPoints.length < 2 ||
-        abs(this._latitude) > MAX_UNEVEN_HOUR_LATITUDE) {
-      this.dayAreaMask = '';
-      this.outerSunriseAngle = null;
-      return;
-    }
-
-    const x1 = outerPoints[0].x;
-    const y1 = outerPoints[0].y;
-    const r2 = this.horizonR;
-    const x2 = innerPoints[0].x;
-    const y2 = innerPoints[0].y;
-    const r3 = inner;
-    const x3 = innerPoints[1].x;
-    const y3 = innerPoints[1].y;
-    const r4 = this.horizonR;
-    const x4 = outerPoints[1].x;
-    const y4 = outerPoints[1].y;
-    const r5 = outerR;
-
-    this.dayAreaMask = `M${x1} ${y1} A${r2} ${r2} 0 0 0 ${x2} ${y2}`;
-
-    if (outerR === CLOCK_RADIUS)
-      this.dayAreaMask += `A${r3} ${r3} 0 0 0 ${x3} ${y3} `;
-
-    this.dayAreaMask += `A${r4} ${r4} 0 0 0 ${x4} ${y4}A${r5} ${r5} 0 1 1 ${x1} ${y1}`;
-
-    this.outerSunriseAngle = atan2_deg(y1, x1);
-    this.innerSunriseAngle = atan2_deg(y2, x2);
-    this.equatorSunriseAngle = atan2_deg(equatorPoints[0].y, equatorPoints[0].x);
-  }
-
   updateTime(forceUpdate = false): void {
     if (!this.observer)
       return;
@@ -1050,7 +854,7 @@ export class AppComponent implements OnInit, SettingsHolder {
     const jde = basicPositions._jde;
 
     this.lastWallTime = basicPositions._date?.wallTime;
-    forEach(basicPositions as any, (key, value) => bpKey(key) && ((this as any)['true_' + key] = value));
+    forEach(basicPositions as any, (key, value) => basicPosKey(key) && ((this as any)['true_' + key] = value));
 
     this.mercuryAngle = adjustForEclipticWheel(solarSystem.getEclipticPosition(MERCURY, jde).longitude.degrees);
     this.venusAngle = adjustForEclipticWheel(solarSystem.getEclipticPosition(VENUS, jde).longitude.degrees);
@@ -1064,10 +868,10 @@ export class AppComponent implements OnInit, SettingsHolder {
         this.adjustMechanicalTimingReference();
 
       forEach(calculateMechanicalPositions(this.time, this.timing, this.timingReference) as any,
-        (key, value) => bpKey(key) && ((this as any)[key] = value));
+        (key, value) => basicPosKey(key) && ((this as any)[key] = value));
     }
     else
-      forEach(basicPositions as any, (key, value) => bpKey(key) && ((this as any)[key] = value));
+      forEach(basicPositions as any, (key, value) => basicPosKey(key) && ((this as any)[key] = value));
 
     const format = this.isoFormat ? DATETIME_LOCAL : 'ISS{year:numeric,month:2-digit,day:2-digit,hour:2-digit}';
 
@@ -1152,69 +956,6 @@ export class AppComponent implements OnInit, SettingsHolder {
     const x = (abs(cos_deg(this.moonPhase)) * 12).toFixed(1);
 
     return `M0 -12.0A12.0 12.0 0 0 ${largeArcFlag} 0 12.0A${x} 12.0 0 0 ${sweepFlag} 0 -12.0`;
-  }
-
-  private getAltitudeCircle(alt: number, doPath = false): CircleAttributes {
-    const lat = max(abs(this.observer.latitude.degrees), 0.5);
-    const theta1 = -lat - (90 + alt);
-    const theta2 = -lat + (90 + alt);
-    const x1 = HORIZON_RADIUS * sin_deg(theta1);
-    const y1 = HORIZON_RADIUS * cos_deg(theta1);
-    const x2 = HORIZON_RADIUS * sin_deg(theta2);
-    const y2 = HORIZON_RADIUS * cos_deg(theta2);
-    const ya = y1 * (HORIZON_RADIUS / (HORIZON_RADIUS - x1));
-    const yb = y2 * (HORIZON_RADIUS / (HORIZON_RADIUS - x2));
-    const cy = (ya + yb) / 2;
-    const r = (yb - ya) / 2;
-
-    return {
-      cy,
-      d: doPath && `M 0 ${cy} m ${-r} 0 a ${r},${r} 0 1,1 ${r * 2},0 a ${r},${r} 0 1,1 ${-r * 2},0`,
-      r
-    };
-  }
-
-  private getHourArc(hour: number, asWedge = false, reverse = false): string {
-    if (this.outerSunriseAngle == null)
-      return '';
-
-    let outer = CLOCK_RADIUS;
-    let inner = TROPIC_RADIUS;
-
-    if (this.midnightSunR) {
-      outer = this.midnightSunR;
-      const deltaLat = 90 - 2 * atan_deg(this.midnightSunR / CLOCK_RADIUS);
-      inner = TROPIC_RADIUS * tan_deg((90 + deltaLat) / 2);
-    }
-
-    const h = (this.southern ? hour : 12 - hour);
-    const outerSweep = 180 + this.outerSunriseAngle * 2;
-    const outerAngle = this.outerSunriseAngle - outerSweep / 12 * h;
-    const x1 = outer * cos_deg(outerAngle);
-    const y1 = outer * sin_deg(outerAngle);
-    const equatorSweep = 180 + this.equatorSunriseAngle * 2;
-    const equatorAngle = this.equatorSunriseAngle - equatorSweep / 12 * h;
-    const x2 = EQUATOR_RADIUS * cos_deg(equatorAngle);
-    const y2 = EQUATOR_RADIUS * sin_deg(equatorAngle);
-    const innerSweep = 180 + this.innerSunriseAngle * 2;
-    const innerAngle = this.innerSunriseAngle - innerSweep / 12 * h;
-    const x3 = inner * cos_deg(innerAngle);
-    const y3 = inner * sin_deg(innerAngle);
-    const r = findCircleRadius(x1, y1, x2, y2, x3, y3);
-
-    if (!asWedge && this.southern)
-      reverse = !reverse;
-
-    if (reverse)
-      return `M ${x3} ${y3} A${r} ${r} 0 0 ${h < 6 ? 1 : 0} ${x1} ${y1} `;
-
-    let path = `M ${x1} ${y1} A${r} ${r} 0 0 ${h < 6 ? 0 : 1} ${x3} ${y3}`;
-
-    if (asWedge)
-      path += 'L' + this.getHourArc(hour + sign(hour - 6), false, !this.southern).substring(1) +
-        `A ${outer} ${outer} 0 0 ${h < 6 ? 0 : 1} ${x1} ${y1} Z`;
-
-    return path;
   }
 
   checkIfTimeIsEditable(): void {
